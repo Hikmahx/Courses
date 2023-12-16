63431366c31ed3276ab6b750b49e33fbcf6a7e2e
Hikmahx <hikmayousuph@gmail.com>
2023-12-16 07:46:08 +0100

    role field added to model

--- models/User.model.js
+++ models/User.model.js
@@ -6,7 +6,11 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    default: "student",
+  },
 });
 
 const UserModel = mongoose.model('User', UserSchema);
